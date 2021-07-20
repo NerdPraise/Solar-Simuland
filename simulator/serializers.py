a5b74b9ed91259ff6cab4c2940ac44feb43dd6f4
NerdPraise <praiseajayi2@gmail.com>
2021-07-20 22:25:24 +0100

    Update register case

--- simulator/serializers.py
+++ simulator/serializers.py
@@ -8,7 +8,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'is_vendor',
-                  'is_engineer', 'email', 'is_active', 'first_name', 'last_name')
+                  'is_engineer', 'email', 'is_active', 'first_name', 'last_name', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
